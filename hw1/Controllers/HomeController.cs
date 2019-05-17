bb70c3cf054efe076870f1bacdca7f2b052ca629
Hou Teng Edward Lam <lamhoutenged@seattleu.edu>
2019-05-17 01:01:35 -0700

    Added Model to Home Controller

--- hw1/Controllers/HomeController.cs
+++ hw1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using hw1.Models;
 
 namespace hw1.Controllers
 {
@@ -10,6 +11,12 @@ namespace hw1.Controllers
     {
         public ActionResult Index()
         {
+            var myData = new HomeViewModel
+            {
+                PhoneCount = 10,
+                UserCount = 15,
+                LastAccessDate = DateTime.Now
+            };
             return View();
         }
 
